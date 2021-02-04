45027ba786c8d976bf9bbe18fae786d7efa26c3c
devinhennayake <78369899+devinhennayake@users.noreply.github.com>
2021-02-04 18:47:09 -0500

    Add files via upload

---
+++ Llama Stacks/LlamaNode.h
@@ -0,0 +1,47 @@
+#ifndef _LLAMANODE_H_
+#define _LLAMANODE_H_
+
+
+/* File: LlamaNode.h
+
+   This file has the class declaration for the LlamaNode class
+   for Project 1
+
+*/
+
+
+#include <iostream>
+using namespace std ;
+
+
+template <class T, int LN_SIZE>   // forward class declaration
+class Llama ;
+
+
+template <class T, int LN_SIZE>
+class LlamaNode {
+
+   friend class Llama<T,LN_SIZE> ;
+
+   public:
+
+   LlamaNode()  ;
+   ~LlamaNode() ;
+
+   static void report() ; 
+
+
+   private:   
+
+   static int newCount ;      // # of times constructor was called
+   static int deleteCount ;   // # of times desctructor was called
+
+   T arr[LN_SIZE] ;
+   LlamaNode *m_next ;   
+
+} ;
+
+
+#include "LlamaNode.cpp"
+
+#endif
